d02fefc24c2de0e979777790f40dcfffee9af21e
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-09-10 02:21:31 +0530

    react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+import Chair from "./pages/Chair.jsx";
+import Products from "./pages/Products.jsx";
+import Sofa from "./pages/Sofa.jsx";
+import Table from "./pages/Table.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/react-vite/",
+    element: <App />,
+    children: [
+      {
+        path: "/react-vite/",
+        element: <Home />,
+      },
+      {
+        path: "/react-vite/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/react-vite/products",
+        element: <Products />,
+      },
+      {
+        path: "/react-vite/chair",
+        element: <Chair />,
+      },
+      {
+        path: "/react-vite/sofa",
+        element: <Sofa />,
+      },
+      {
+        path: "/react-vite/table",
+        element: <Table />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
